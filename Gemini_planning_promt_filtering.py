791e4d3d5b5f94cb25e2468bc5f841c2acf245ab
NirjharDebnath <nirjhardebnath2006@gmail.com>
2024-10-02 23:31:23 +0530

    Update Gemini_planning_promt_filtering.py

--- Gemini_planning_promt_filtering.py
+++ Gemini_planning_promt_filtering.py
@@ -1,13 +1,8 @@
-"""
-Install the Google AI Python SDK
-
-$ pip install google-generativeai
-"""
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
 
-load_dotenv()
+load_dotenv(
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Create the model
